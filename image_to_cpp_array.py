0f973eec6475d6968be14f5817bbad7ab6bd5559
Riley Weber <rileyw13@protonmail.com>
2019-10-04 10:25:41 -0400

    created python script to convert image to csv and generate expected sobel filter

---
+++ image_to_cpp_array.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from skimage import filters, io
+
+def image_to_csv(image_array: np.ndarray, output_filename: str):
+    with open(output_filename, "w") as f:
+        for row in image_array:
+            output_string = ''
+            for pixel in row:
+                output_string += str(pixel) + ','
+
+            output_string = output_string[:-2]
+            output_string += '\n'
+            f.write(output_string)
+                
+def main():
+    filename = 'saturn-v-2048x2048-bw'
+    image = io.imread(filename + '.jpg')
+    image_to_csv(image, filename + '.csv')
+    edges = filters.sobel(image)
+
+if __name__ == "__main__":
+    main()
