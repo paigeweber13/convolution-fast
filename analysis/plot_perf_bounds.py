816b0dec3534dedf62006e48e9a19f40f33e5423
Riley Weber <rileyw13@protonmail.com>
2019-10-04 11:31:37 -0400

    touching up the plot

--- analysis/plot_perf_bounds.py
+++ analysis/plot_perf_bounds.py
@@ -4,20 +4,27 @@ def flop_perf_bound(m, n, k):
     return n * m/(n * m * 2 * k * k /1.64e12)
 
 def mem_perf_bound(m, n, k, datatype_size):
-    return n * m/(((n * m) * datatype_size/3.3e10) + (k*k*datatype_size/1e11))
+    return n * m/((n * m * datatype_size/3.3e10) + (k*k*datatype_size/1e11))
 
 def plot_as_function_of_k(m, n, datatype_size):
     x = []
     y_flop = []
     y_mem = []
-    for k in range(1,20):
+    for k in range(1,21):
         x.append(k)
         y_flop.append(flop_perf_bound(m,n,k))
         y_mem.append(mem_perf_bound(m,n,k,datatype_size))
 
+    print(y_mem)
     plt.figure(figsize=(15,7))
+    plt.yscale('log')
+    plt.title('Performance Bounds as Functions of Convolution Dimension')
+    plt.ylabel('Pixels/s')
+    plt.xlabel('k')
     plt.plot(x, y_flop)
     plt.plot(x, y_mem)
+    plt.legend(['Flop Performance Bound', 'Memory Performance Bound'])
+    plt.xticks(range(1, 21))
     plt.show()
 
 if __name__ == '__main__':
