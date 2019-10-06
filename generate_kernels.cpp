b5a4e54460a73a2b426b89d9ebe9c4eda24de1d3
Riley <rileyw13@protonmail.com>
2019-10-06 17:25:46 -0400

    added comments and fixed code vectors are now actually allocated beforehand added print function

--- generate_kernels.cpp
+++ generate_kernels.cpp
@@ -1,28 +1,51 @@
+#include <iomanip>
+#include <iostream>
 #include <math.h>
 #include <vector>
 
 using namespace std;
+
+// a kernel is just a vector<vector<float>>
+// elements are accessed with kernel[row][column]
 
 float bivariate_gaussian(float std_dev, float x, float y){
   return exp(-(pow(x, 2) + pow(y, 2))/(2*pow(std_dev,2)));
 }
 
 vector<vector<float>> generate_kernel(size_t k){
+  cout << "Gerneating single kernel!" << endl;
   vector<vector<float>> result;
   // will need tuning
-  float std_dev = k * 2/3;
-  for(size_t i = 0; i < k; i++){
-    for(size_t j = 0; j < k; j++){
-      result[i][j] = bivariate_gaussian(std_dev, i, j);
+  float std_dev = float(k) * 2/3;
+
+  unsigned half = ceil(k/2);
+  for(size_t row = 0; row < k; row++){
+    result.push_back(vector<float>(k));
+    for(size_t column = 0; column < k; column++){
+      cout << "row: " << row << " column: " << column << endl;
+      result[row][column] = bivariate_gaussian(std_dev, column-half, row-half);
     }
   }
   return result;
 }
 
 vector<vector<vector<float>>> generate_kernels(size_t max_k){
-  vector<vector<vector<float>>> result;
-  for(size_t i = 1; i <= k; i += 2){
+  cout << "Gerneating a bunch of kernels!" << endl;
+  vector<vector<vector<float>>> result(max_k+1);
+  for(size_t i = 1; i <= max_k; i += 2){
     result[i] = generate_kernel(i);
   }
   return result;
+}
+
+void print_kernel(vector<vector<float>> kernel){
+  cout << "printing single kernel" << endl;
+  setprecision(4);
+  for(auto row : kernel){
+    for(auto element : row){
+      cout << element << "  ";
+    }
+    cout << endl;
+  }
+  cout << endl;
 }
