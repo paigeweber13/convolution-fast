7892cf5cecf36a0b9beecb83653cf0f3543c164a
Riley <rileyw13@protonmail.com>
2019-10-06 17:27:15 -0400

    removed debug prints, now using ints

--- generate_kernels.cpp
+++ generate_kernels.cpp
@@ -13,16 +13,14 @@ float bivariate_gaussian(float std_dev, float x, float y){
 }
 
 vector<vector<float>> generate_kernel(size_t k){
-  cout << "Gerneating single kernel!" << endl;
   vector<vector<float>> result;
   // will need tuning
   float std_dev = float(k) * 2/3;
 
-  unsigned half = ceil(k/2);
-  for(size_t row = 0; row < k; row++){
+  int half = ceil(k/2);
+  for(int row = 0; row < k; row++){
     result.push_back(vector<float>(k));
-    for(size_t column = 0; column < k; column++){
-      cout << "row: " << row << " column: " << column << endl;
+    for(int column = 0; column < k; column++){
       result[row][column] = bivariate_gaussian(std_dev, column-half, row-half);
     }
   }
@@ -30,7 +28,6 @@ vector<vector<float>> generate_kernel(size_t k){
 }
 
 vector<vector<vector<float>>> generate_kernels(size_t max_k){
-  cout << "Gerneating a bunch of kernels!" << endl;
   vector<vector<vector<float>>> result(max_k+1);
   for(size_t i = 1; i <= max_k; i += 2){
     result[i] = generate_kernel(i);
@@ -39,7 +36,6 @@ vector<vector<vector<float>>> generate_kernels(size_t max_k){
 }
 
 void print_kernel(vector<vector<float>> kernel){
-  cout << "printing single kernel" << endl;
   setprecision(4);
   for(auto row : kernel){
     for(auto element : row){
