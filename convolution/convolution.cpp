052e9ef98ae3f8e2e69eb7576ebf6128a0e4ecdb
Riley <rileyw13@protonmail.com>
2019-10-07 22:04:52 -0400

    finished for loops, added usage info. INCOMPLETE

--- convolution/convolution.cpp
+++ convolution/convolution.cpp
@@ -13,12 +13,25 @@ vector<vector<uint8_t>> convolve(vector<vector<uint8_t>> image, Kernel kernel){
       // for each row
       for(size_t j = m; j < image[i].size() - m; j++){
         // for each pixel in that row
-
+        float sum = 0;
+        for(int k = -m; k <= m; k++){
+          for(int l = -m; l <= m; l++){
+            sum += image[i+k][j+k] * kernel.get(k, l);
+          }
+        }
+        output_image[i][j] = uint8_t(sum);
       }
     }
   }
+
+  return output_image;
 }
 
 int main(int argc, char** argv){
+  // two possible usages
+  auto usage1 = argv[0] + " n m k";
+  auto usage2 = argv[0] + " input_image_data.csv";
+
+  if(argc < )
   return 0;
 }
