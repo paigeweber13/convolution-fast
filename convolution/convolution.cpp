3ba3f4ae97062bd20b1d13260052879eac585849
Riley <rileyw13@protonmail.com>
2019-10-07 21:58:01 -0400

    added output image, omp parallel block, main

--- convolution/convolution.cpp
+++ convolution/convolution.cpp
@@ -1,10 +1,24 @@
 #include "convolution.h"
 
 vector<vector<uint8_t>> convolve(vector<vector<uint8_t>> image, Kernel kernel){
-  for(size_t i = 0; i < image.size(); i++){
-    // for each row
-    for(size_t j = 0; j < image[i].size(); j++){
-      // for each pixel in that row
+  auto m = kernel.get_midpoint();
+  // create new output image
+  auto output_image = vector<vector<uint8_t>>(image);
+
+  #pragma omp parallel
+  {
+    // don't compute edges
+    #pragma omp for
+    for(size_t i = m; i < image.size() - m; i++){
+      // for each row
+      for(size_t j = m; j < image[i].size() - m; j++){
+        // for each pixel in that row
+
+      }
     }
   }
+}
+
+int main(int argc, char** argv){
+  return 0;
 }
