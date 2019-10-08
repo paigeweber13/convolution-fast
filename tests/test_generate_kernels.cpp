fb5fe4d984148e31d013b96c9ce0a61094891346
Riley <rileyw13@protonmail.com>
2019-10-07 21:24:13 -0400

    makefile, test use new kernel code

--- tests/test_generate_kernels.cpp
+++ tests/test_generate_kernels.cpp
@@ -1,10 +1,12 @@
-#include "../generate_kernels.cpp"
+#include "../convolution/generate_kernels.h"
+#include <iostream>
 
 int main(int argc, char* argv[]){
-  vector<vector<vector<float>>> kernels = generate_kernels(9);
+  size_t max_k = 9;
+  vector<Kernel> kernels = generate_blur_kernels(max_k);
 
-  for(auto kernel : kernels){
-    print_kernel(kernel);
+  for(size_t i = 3; i <= max_k; i += 2){
+    cout << kernels[i].to_string();
   }
 
   return 0;
