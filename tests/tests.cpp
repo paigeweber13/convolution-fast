eb635b0556ea38b12f46ebaf583de198523cd28b
Riley <rileyw13@protonmail.com>
2019-10-08 20:16:32 -0400

    edited so that future tests may be added

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,13 +1,18 @@
 #include "../convolution/generate_kernels.h"
+#include "../convolution/convolution.h"
 #include <iostream>
 
-int main(int argc, char* argv[]){
+void test_generate_kernels(){
   size_t max_k = 9;
   vector<Kernel> kernels = generate_blur_kernels(max_k);
 
   for(size_t i = 3; i <= max_k; i += 2){
     cout << kernels[i].to_string();
   }
+}
+
+int main(int argc, char* argv[]){
+  test_generate_kernels();
 
   return 0;
 }
