453f9a68511b6e4cc450523d09fcb3b7aae32ae7
Riley Weber <rileyw13@protonmail.com>
2019-10-10 13:19:18 -0400

    input image must now be supplied as a parameter
    duration now in milliseconds
    reusing images between tests

--- convolution/main.cpp
+++ convolution/main.cpp
@@ -8,10 +8,11 @@
 
 using namespace std;
 
-double time_single_test(unsigned m, unsigned n, unsigned k){
+double time_single_test(vector<vector<uint8_t>> image, unsigned m, unsigned n,
+                        unsigned k){
     // generate dummy image of size m, n and kernel of size k. Convolve them
     // and report time.
-    auto image = generate_garbage_image(m, n);
+    // auto image = generate_garbage_image(m, n);
     auto kernel = generate_blur_kernel(k);
 
     auto start_time = chrono::high_resolution_clock::now();
@@ -20,8 +21,8 @@ double time_single_test(unsigned m, unsigned n, unsigned k){
 
     auto end_time = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time).count();
-    double duration_seconds = duration/10e9;
-    return duration_seconds;
+    double duration_milliseconds = duration/10e6;
+    return duration_milliseconds;
 }
 
 void test_and_output_various_sizes(){
@@ -37,14 +38,17 @@ void test_and_output_various_sizes(){
   unsigned kernel_sizes[7] = {3, 5, 7, 9, 11, 13, 15};
 
   // m is length, n is height
-  cout << "m,n,k,time (s)" << endl;
+  cout << "m,n,k,time (ms)" << endl;
+  // maximum size for all images. Reuse image between tests
+
   for (auto image_size : image_sizes){
+    auto m = image_size[0];
+    auto n = image_size[1];
+    auto image = generate_garbage_image(m, n);
     for (auto kernel_size : kernel_sizes){
-      auto m = image_size[0];
-      auto n = image_size[1];
       cout << to_string(m) << "," << to_string(n) << ","
            << to_string(kernel_size) << ","
-           << to_string(time_single_test(m,n,kernel_size)) << endl;
+           << to_string(time_single_test(image, m,n,kernel_size)) << endl;
     }
   }
 }
