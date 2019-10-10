57f68b65e6d33cf6f4d96f59736de3bdb4366d83
Riley Weber <rileyw13@protonmail.com>
2019-10-10 12:44:34 -0400

    created separate functions for different functionality.
    Now tests all image sizes.

--- convolution/main.cpp
+++ convolution/main.cpp
@@ -8,34 +8,59 @@
 
 using namespace std;
 
+double time_single_test(unsigned m, unsigned n, unsigned k){
+    // generate dummy image of size m, n and kernel of size k. Convolve them
+    // and report time.
+    auto image = generate_garbage_image(m, n);
+    auto kernel = generate_blur_kernel(k);
+
+    auto start_time = chrono::high_resolution_clock::now();
+
+    auto output = convolve(image, kernel);
+
+    auto end_time = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time).count();
+    double duration_seconds = duration/10e9;
+    return duration_seconds;
+}
+
+void test_and_output_various_sizes(){
+
+  unsigned image_sizes[5][2] = {
+      {1024, 768},
+      {2048, 2048},
+      {8192, 8192},
+      {4194304, 768},
+      {16777216, 768}
+  };
+
+  unsigned kernel_sizes[7] = {3, 5, 7, 9, 11, 13, 15};
+
+  // m is length, n is height
+  cout << "m,n,k,time (s)" << endl;
+  for (auto image_size : image_sizes){
+    for (auto kernel_size : kernel_sizes){
+      auto m = image_size[0];
+      auto n = image_size[1];
+      cout << to_string(m) << "," << to_string(n) << ","
+           << to_string(kernel_size) << ","
+           << to_string(time_single_test(m,n,kernel_size)) << endl;
+    }
+  }
+}
+
 int main(int argc, char** argv){
   // two possible usages
-  string usage1 = string(argv[0]) + " m n k";
+  string usage1 = string(argv[0]);
   string usage2 = string(argv[0]) + " input_image_data.csv";
 
-  if(argc < 2){
-    cout << "usages:" << endl;
-    cout << usage1 << endl;
-    cout << usage2 << endl;
-  } else if(argc < 4){
+  if(argc > 1){
     auto image = load_image(argv[1]);
     // auto edges = convolve(image, generate_sobel_v_kernel());
     auto edges = convolve(image, generate_blur_kernel(5));
     save_image(edges, "output.csv");
   } else {
-    // generate dummy image of size n, m and kernel of size k. Convolve them
-    // and report time.
-    auto image = generate_garbage_image(stoi(argv[1]), stoi(argv[2]));
-    auto kernel = generate_blur_kernel(stoi(argv[3]));
-
-    auto start_time = std::chrono::high_resolution_clock::now();
-
-    auto output = convolve(image, kernel);
-
-    auto end_time = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
-    double duration_seconds = duration/10e9;
-    cout << to_string(duration_seconds) << endl;
+    test_and_output_various_sizes();
   }
 
   return 0;
