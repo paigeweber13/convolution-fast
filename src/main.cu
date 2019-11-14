7ea262a7b6151d7c48db17cdfe0050cf628b1eac
Riley Weber <rileyw13@protonmail.com>
2019-11-14 17:24:23 -0500

    using getopt instead of boost program options

--- src/main.cu
+++ src/main.cu
@@ -1,44 +1,108 @@
 #include <boost/program_options.hpp>
 #include <chrono>
+#include <getopt.h>
 #include <iostream>
 #include <string>
 
 #include "convolution.h"
+#include "image.h"
+#include "kernel.h"
 
 using namespace std;
 namespace po = boost::program_options;
 
 // function prototypes
+void print_help();
 void time_single_gpu_test(size_t m, size_t n, size_t k);
-double time_single_cpu_test(float** image, float** output, unsigned m,
-                            unsigned n, unsigned k);
+double time_single_cpu_test(unsigned m, unsigned n, unsigned k);
 po::options_description get_program_options_desc();
 void test_blur_image(string input_filename, string output_filename);
 
 int main(int argc, char** argv){
   auto desc = get_program_options_desc();
 
-  po::variables_map vm;
-  po::store(po::parse_command_line(argc, argv, desc), vm);
-  po::notify(vm);
+  const char* const short_opts = "ha:i:o::s";
+  const option long_opts[] = {
+    {"help", no_argument, nullptr, 'h'},
+    {"arch", required_argument, nullptr, 'a'},
+    {"image-input", required_argument, nullptr, 'i'},
+    {"image-output", optional_argument, nullptr, 'o'},
+    // 3 arguments should be provided, they will be deferred to the end
+    {"speedtest", no_argument, nullptr, 's'}, 
+  };
 
-  if (vm.count("help")) {
-    cout << desc << endl;
-    return 1;
-  } 
-  else if (vm.count("image-input")) {
-    cout << "image-input" << endl;
+  char arch = 'c'; 
+  string image_input = "";
+  string image_output = "output.pgm";
+  bool speedtest = false;
+
+  auto opt = getopt_long(argc, argv, short_opts, long_opts, nullptr);
+  while (opt != -1) {
+    switch (opt){
+      case 'a':
+        arch = *optarg;
+        break;
+      case 'i':
+        image_input = *optarg;
+        break;
+      case 'o':
+        if(optarg != 0){
+          image_output = *optarg;
+        }
+        break;
+      case 's':
+        speedtest = true;
+        break;
+
+      case 'h':
+      default:
+        print_help();
+        break;
+    }
+    opt = getopt_long(argc, argv, short_opts, long_opts, nullptr);
   }
-  else if (vm.count("speedtest")) {
-    cout << "speedtest" << endl;
+
+  if (speedtest) {
+    auto m = stoul(argv[optind]);
+    auto n = stoul(argv[optind+1]);
+    auto k = stoul(argv[optind+2]);
+
+    switch (arch) {
+      case 'c':
+        time_single_cpu_test(m, n, k);
+        break;
+      case 'g':
+        break;
+      default:
+        cout << "invalid architecture specified!" << endl;
+        return 2;
+        break;
+    }
   }
 
   return 0;
 }
 
+void print_help(){
+  std::cout <<
+    "-h, --help:                    Show help\n"
+    "-a, --arch <char>:             indicate if code should be run on cpu "
+                                    "(c) or gpu (g)\n"
+    "-i, --image-input <filename>:  image to blur\n"
+    "-o, --image-output <filename>: place to save image (has no effect "
+                                    "unless --image-input is also specified\n"
+                                    "note: for both image-input and "
+                                    "image-output, only ascii .pgm files are "
+                                    "supported."
+    "-s, --speedtest <m> <n> <k>:   time a test with given image size m, n "
+                                    "and kernel size k\n";
+  exit(1);
+}
+
 void time_single_gpu_test(size_t m, size_t n, size_t k){
-  auto input = generate_garbage_image(m, n);
-  auto output = generate_garbage_image(m, n);
+  auto input = Image(m, n);
+  input.randomize();
+  auto output = Image(m, n);
   Kernel kernel(k);
   kernel.make_blur_kernel();
 
@@ -47,50 +111,34 @@ void time_single_gpu_test(size_t m, size_t n, size_t k){
   cout << setw(10) << m << setw(10) << n << setw(10) << k << endl;
 }
 
-double time_single_cpu_test(float** image, float** output, unsigned m,
-                            unsigned n, unsigned k){
-    // Convolve them and report time.
-    size_t num_runs = 1;
-    Kernel kernel(k);
-    kernel.make_blur_kernel();
+double time_single_cpu_test(unsigned m, unsigned n, unsigned k){
+  // Convolve them and report time.
+  size_t num_runs = 1;
+  auto input = Image(m, n);
+  input.randomize();
+  auto output = Image(m, n);
+  Kernel kernel(k);
+  kernel.make_blur_kernel();
 
-    auto start_time = chrono::high_resolution_clock::now();
+  auto start_time = chrono::high_resolution_clock::now();
 
-    for(size_t i = 0; i < num_runs; i++){
-      convolve(image, output, kernel);
-    }
+  for(size_t i = 0; i < num_runs; i++){
+    convolve(input, output, kernel);
+  }
 
-    auto end_time = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time).count();
-    double duration_seconds = duration/(num_runs*1e9);
-    return duration_seconds;
-}
-
-po::options_description get_program_options_desc(){
-  po::options_description desc("Allowed options");
-  desc.add_options()
-    ("help,h", "display this help message")
-    ("arch,a", po::value<char>()->default_value('c'),
-        "select architecture to use (c for cpu, g for gpu)")
-    ("image-input,imi", po::value<string>(), "image to blur convolve")
-    ("image-output,imo", po::value<string>()->default_value(
-        string("output.pgm")),
-        "file to output image (in ascii pgm format). This does nothing "
-        "unless image-input is also specified")
-    ("speedtest,s", po::value<vector<size_t>>(),
-     "m, n, k (m, n are width, height of image and k is size of blur kernel")
-  ;
-
-  return desc;
+  auto end_time = chrono::high_resolution_clock::now();
+  auto duration = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time).count();
+  double duration_seconds = duration/(num_runs*1e9);
+  return duration_seconds;
 }
 
 void test_blur_image(string input_filename, string output_filename){
   cout << "Testing blur kernel on image " << input_filename << endl;
 
   cout << "loading image..." << endl;
-  auto image = load_image(input_filename);
+  auto image = Image::load_image(input_filename);
   // creating output for blurred
-  auto blurred = load_image(input_filename);
+  auto blurred = Image(image.get_m(), image.get_n());
   cout << "finished loading!" << endl;
 
   cout << "blurring..." << endl;
@@ -101,7 +149,7 @@ void test_blur_image(string input_filename, string output_filename){
   cout << "finished blurring!" << endl;
 
   cout << "saving image to " << output_filename << endl;
-  save_image(blurred, output_filename);
+  Image::save_image(blurred, output_filename);
   cout << "finished saving!" << endl;
 }
 
