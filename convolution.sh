1d2f0aeda3bfdc4098eda71581db0ab1afb94385
Riley Weber <rileyw13@protonmail.com>
2019-11-15 18:07:18 -0500

    improved output

--- convolution.sh
+++ convolution.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
+echo "------ CPU: ------"
+echo "running tests"
 ./convolution.out -t tests/saturn-v-2048x2048-bw.pgm
+echo ""
+echo "convolving actual image"
 ./convolution.out -i tests/saturn-v-2048x2048-bw.pgm -o tests/saturn-v-out.pgm
-./convolution.out -s 1024 1024 7
+echo ""
  
+echo "------ GPU: ------"
+echo "running tests"
 # ./convolution.out -g g -t tests/saturn-v-2048x2048-bw.pgm
+echo ""
+echo "convolving actual image"
 # ./convolution.out -a g -i tests/saturn-v-2048x2048-bw.pgm -o tests/saturn-v-out.pgm
+echo ""
+
+echo "running speedtests"
+echo "------ CPU: ------"
+echo "        m,        n,   k,   time (s), performance (Mp/s)"
+./convolution.out -s 1024 1024 7
+
+echo "------ GPU: ------"
+echo "        m,        n,   k,   time (s), performance (Mp/s)"
 # ./convolution.out -a g -s 1024 1024 7
 
