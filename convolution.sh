5a4f9984aaa246f43eeab4255e74ffc510a11608
Riley Weber <rileyw13@protonmail.com>
2019-11-15 16:56:21 -0500

    added test cases to script

--- convolution.sh
+++ convolution.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+./convolution.out -t tests/saturn-v-2048x2048-bw.pgm
 ./convolution.out -i tests/saturn-v-2048x2048-bw.pgm -o tests/saturn-v-out.pgm
 ./convolution.out -s 1024 1024 7
  
+# ./convolution.out -g g -t tests/saturn-v-2048x2048-bw.pgm
 # ./convolution.out -a g -i tests/saturn-v-2048x2048-bw.pgm -o tests/saturn-v-out.pgm
 # ./convolution.out -a g -s 1024 1024 7
 
